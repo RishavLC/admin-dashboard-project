f829c629f3e1051dce6f97614d6d1337c9cf6af4
RishavLC <shrestharishav444@gmail.com>
2025-06-30 23:14:18 +0545

    logout added

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,21 +1,18 @@
-import Sidebar from '../components/Sidebar';
 import { Layout } from 'antd';
+import Sidebar from '../components/Sidebar';
+
 const { Sider, Content } = Layout;
 
-const Dashboard = () => {
-  return (
-    <Layout style={{ minHeight: '100vh' }}>
-      <Sider>
-        <Sidebar />
-      </Sider>
-      <Layout>
-        <Content style={{ padding: '20px' }}>
-          <h1>Welcome to QuickFixPro Admin Dashboard</h1>
-          <p>Here you can manage users, services, and view reports.</p>
-        </Content>
-      </Layout>
+const Dashboard = ({ setIsAuthenticated }) => (
+  <Layout style={{ minHeight: '100vh' }}>
+    <Sider><Sidebar setIsAuthenticated={setIsAuthenticated} /></Sider>
+    <Layout>
+      <Content style={{ padding: '20px' }}>
+        <h1>Welcome to QuickFixPro Admin Dashboard</h1>
+        <p>Monitor services, users, and reports here.</p>
+      </Content>
     </Layout>
-  );
-};
+  </Layout>
+);
 
 export default Dashboard;
